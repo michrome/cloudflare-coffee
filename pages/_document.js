9bd834666bdc9e958f5d924596471950ac30981a
Mike Jerome <m@j4e.name>
2021-05-12 22:52:12 +0100

    Add mug logo

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,14 @@ class MyDocument extends Document {
         <body>
           <div className="px-4 py-10 max-w-3xl mx-auto sm:px-6 sm:py-12 lg:max-w-4xl lg:py-16 lg:px-8 xl:max-w-6xl">
             <article className="prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto">
-              <h1>cloudflare.coffee</h1>
+              <h1 className="hidden">cloudflare.coffee</h1>
+              <img
+                src="/mug.png"
+                width={380}
+                height={321}
+                className="h-28 w-auto filter drop-shadow-mug"
+                alt="Coffee mug with 1.1.1.1 typed on it"
+              />
               <Main />
             </article>
           </div>

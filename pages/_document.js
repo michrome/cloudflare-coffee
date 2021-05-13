40ea5082cefbdacc9f7da3f8094f72b4c9cf375d
Mike Jerome <m@j4e.name>
2021-05-13 14:51:32 +0100

    Apply filter directly in style

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ class MyDocument extends Document {
         <body>
           <div className="px-4 py-4 max-w-3xl mx-auto sm:px-6 sm:py-12 lg:max-w-4xl lg:py-16 lg:px-8 xl:max-w-6xl">
             <article className="prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto">
-              <header className="filter drop-shadow-cf-orange">
+              <header>
                 <h1 className="hidden">cloudflare.coffee</h1>
                 <img
                   src="/mug.png"
@@ -21,6 +21,9 @@ class MyDocument extends Document {
                   height={321}
                   className="h-14 sm:h-28 w-auto"
                   alt="Coffee mug with 1.1.1.1 typed on it"
+                  style={{
+                    filter: "drop-shadow(0 10px 25px #F5821F)",
+                  }}
                 />
               </header>
               <Main />
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      dropShadow: {
-        "cf-orange": "0 10px 25px #F5821F",
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},

f21f8e338b216acfa2e532d5ca6cffbe9d53e89e
Mike Jerome <m@j4e.name>
2021-05-13 16:11:21 +0100

    Preload the mug but don't cache css files

--- next.config.js
+++ next.config.js
@@ -10,15 +10,6 @@ module.exports = {
           },
         ],
       },
-      {
-        source: "/:slug(.*[.]css)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "max-age=31536000",
-          },
-        ],
-      },
     ];
   },
 };

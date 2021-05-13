1fce5abf9bba8b8d67ba956692a76e72acf95dfd
Mike Jerome <m@j4e.name>
2021-05-13 16:47:45 +0100

    Set Favicon to be coffee cup

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,11 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <link rel="preload" href="/mug.png" as="image" />
-        <title>☕️ cloudflare.coffee</title>
+        <title>cloudflare.coffee</title>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>☕️</text></svg>"
+        />
       </Head>
       <Component {...pageProps} />
     </>

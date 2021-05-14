f0ab69088dad49f3700833b0d0a633af2bcbadd3
Mike Jerome <m@j4e.name>
2021-05-14 09:57:33 +0100

    Remove div

--- pages/_document.js
+++ pages/_document.js
@@ -11,20 +11,18 @@ class MyDocument extends Document {
     return (
       <Html lang="en-GB">
         <Head />
-        <body>
-          <div className="px-4 py-4 max-w-3xl mx-auto sm:px-6 sm:py-12 lg:max-w-4xl lg:py-16 lg:px-8 xl:max-w-6xl">
-            <div className="prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto">
-              <header>
-                <h1 className="hidden">cloudflare.coffee</h1>
-                <Mug />
-              </header>
-              <main>
-                <Main />
-              </main>
-              <footer>
-                <Mug />
-              </footer>
-            </div>
+        <body className="px-4 py-4 max-w-3xl mx-auto sm:px-6 sm:py-12 lg:max-w-4xl lg:py-16 lg:px-8 xl:max-w-6xl">
+          <div className="prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto">
+            <header>
+              <h1 className="hidden">cloudflare.coffee</h1>
+              <Mug />
+            </header>
+            <main>
+              <Main />
+            </main>
+            <footer>
+              <Mug />
+            </footer>
           </div>
           <NextScript />
         </body>

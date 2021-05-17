b52cb8c6045930e22b17182eddda1975843da894
Mike Jerome <m@j4e.name>
2021-05-17 17:49:59 +0100

    Whitelist styles used by worker

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,8 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    purgecss: {
+      whitelist: ["focus:outline-none", "whitespace-nowrap", "ml-10"],
+    },
   },
-}
+};

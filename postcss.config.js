429d1023b347a24b2a3b78dcf6ddb312999791b5
Mike Jerome <m@j4e.name>
2021-05-17 21:17:54 +0100

    Revert postcss conf

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,5 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    "@fullhuman/postcss-purgecss": {
-      whitelist: ["focus:outline-none", "whitespace-nowrap", "ml-10"],
-    },
   },
 };

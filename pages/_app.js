e141cd4e596cef2ab8d50b03bc804bffb455976d
Mike Jerome <m@j4e.name>
2021-05-25 16:27:16 +0100

    Add presentation

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 // import App from 'next/app'
-import "tailwindcss/tailwind.css";
+import "../styles/globals.css";
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      "cf-orange": "#F5821F",
+    },
     extend: {
       dropShadow: {
         "cf-orange": "0 10px 25px #F5821F",
